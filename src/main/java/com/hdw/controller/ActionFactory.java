0bbb20311fdd90d232e8460696c2b9789df57574
dd2558 <dd2558@naver.com>
2024-03-18 01:01:20 +0900

    BoardWriteAction 추가

--- src/main/java/com/hdw/controller/ActionFactory.java
+++ src/main/java/com/hdw/controller/ActionFactory.java
@@ -2,6 +2,7 @@ package com.hdw.controller;
 
 import com.hdw.controller.action.Action;
 import com.hdw.controller.action.BoardListAction;
+import com.hdw.controller.action.BoardWriteFormAction;
 
 public class ActionFactory {
 	private static ActionFactory instance = new ActionFactory();
@@ -19,6 +20,8 @@ public class ActionFactory {
 		System.out.println("ActionFactiry :" + command);
 		if(command.equals("board_list")){
 			action = new BoardListAction();
+		}else if(command.equals("board_write_form")) {
+			action = new BoardWriteFormAction();
 		}
 		return action;
 	}
--- src/main/java/com/hdw/controller/action/Action.java
+++ src/main/java/com/hdw/controller/action/Action.java
@@ -1,7 +1,6 @@
 package com.hdw.controller.action;
 
 import java.io.IOException;
-import java.rmi.ServerException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
